4f14b7ef8cadfadba5e3af42d304ad3a7e44f608
Yury <72691412+cheatsnake@users.noreply.github.com>
2021-11-27 19:00:17 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { Application } from "express";
+import { Application, Request, Response } from "express";
 import cors from "cors";
 import apiRouter from "./routes/api.routes";
 import express from "express";
@@ -8,7 +8,9 @@ export default function createServer() {
 
     app.use(cors());
     app.use(express.json());
-    app.use("/", express.static(__dirname + "/../public"));
+    app.use("/", (req: Request, res: Response) => {
+        res.send("Hello World!");
+    });
     app.use("/api", apiRouter);
 
     return app;
